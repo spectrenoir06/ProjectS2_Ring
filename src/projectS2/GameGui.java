2ea446cd64243b51dfd4c758e7121de90bcf65e4
spectrenoir <antoine@doussaud.org>
2014-05-10 19:22:20 +0200

    move img in img/

--- src/projectS2/GameGui.java
+++ src/projectS2/GameGui.java
@@ -4,7 +4,6 @@ import com.modeliosoft.modelio.javadesigner.annotations.objid;
 
 @objid ("5be3a65b-5561-4379-a02c-e665ea556a83")
 public class GameGui extends Game {
-    @objid ("22c47aed-ded1-43c8-a008-15c1c4af9701")
-    public int test;
+
 
 }
